593b53c2be318ab2c199f53b1a489ac48b22c849
Raphael Blanchard <blanchard.raphael.pro@gmail.com>
2023-02-28 07:56:27 +0100

    added path tracking / need to print well

--- src/utils.jl
+++ src/utils.jl
@@ -74,6 +74,10 @@ struct Vertex
     bottom::Int64
 end
 
+struct Coord
+    x::Int64
+    y::Int64
+end
 # Creates a Vertex from a point (i, j) in the matrix charMat
 function transform_vertex(i::Int64, j::Int64, charMat::Matrix{Char})
     # Top border case
@@ -138,28 +142,37 @@ function min_dist_vert(distances::Matrix{Int64}, uncovered_nodes::Matrix{Bool})
 end
 
 # Function that updates the distance values of the neighbouring node at index (i, j)
-function update_distances(distances::Matrix{Int64}, uncovered_nodes::Matrix{Bool}, graph::Matrix{Vertex}, i::Int64, j::Int64, shortest_paths::Matrix{Vector{Tuple{Int64, Int64}}})
+function update_distances(distances::Matrix{Int64}, uncovered_nodes::Matrix{Bool}, graph::Matrix{Vertex}, i::Int64, j::Int64, parents::Matrix{Coord})
     # top neighbour
     if graph[i, j].top > 0 && uncovered_nodes[i-1, j] == false && distances[i-1, j] > distances[i, j] + graph[i, j].top
-        shortest_paths[i-1, j] = [shortest_paths[i,j]; (i-1, j)]
+        parents[i-1, j] = Coord(i, j)
+        # shortest_paths[i-1, j] = [shortest_paths[i,j]; (i-1, j)]
         distances[i-1, j] = distances[i, j] + graph[i, j].top
     end
     # left neighbour
     if graph[i, j].left > 0 && uncovered_nodes[i, j-1] == false && distances[i, j-1] > distances[i, j] + graph[i, j].left
-        shortest_paths[i, j-1] = [shortest_paths[i,j]; (i-1, j)]
+        parents[i, j-1] = Coord(i, j)
+        # shortest_paths[i, j-1] = [shortest_paths[i,j]; (i-1, j)]
         distances[i, j-1] = distances[i, j] + graph[i, j].left
     end
     # right neighbour
     if graph[i, j].right > 0 && uncovered_nodes[i, j+1] == false && distances[i, j+1] > distances[i, j] + graph[i, j].right
-        shortest_paths[i, j+1] = [shortest_paths[i,j]; (i, j+1)]
+        parents[i, j+1] = Coord(i, j)
+        # shortest_paths[i, j+1] = [shortest_paths[i,j]; (i, j+1)]
         distances[i, j+1] = distances[i, j] + graph[i, j].right
     end
     # bottom neighbour
     if graph[i, j].bottom > 0 && uncovered_nodes[i+1, j] == false && distances[i+1, j] > distances[i, j] + graph[i, j].bottom
-        shortest_paths[i+1, j] = [shortest_paths[i,j]; (i+1, j)]
+        parents[i+1, j] = Coord(i, j)
+        # shortest_paths[i+1, j] = [shortest_paths[i,j]; (i+1, j)]
         distances[i+1, j] = distances[i, j] + graph[i, j].bottom
     end
-    return distances, shortest_paths
+    return distances, parents
+end
+
+function print_parents(parents::Matrix{Coord}, curr_x, curr_y)
+    print_parents(parents, parents[curr_x, curr_y].x, parents[curr_x, curr_y].y)
+    println(curr_x, curr_y)
 end
 
 function updated_dijkstra(graph::Matrix{Vertex}, start_x::Int64, start_y::Int64, finish_x::Int64, finish_y::Int64)
@@ -173,6 +186,9 @@ function updated_dijkstra(graph::Matrix{Vertex}, start_x::Int64, start_y::Int64,
     distances::Matrix{Int64} = [typemax(Int64) for i in 1:size(graph,1), j in 1:size(graph,2)]
     distances[start_x, start_y] = 0
 
+    # parents matrix
+    parents = Matrix{Coord}(undef, size(graph, 1), size(graph, 2))
+
     # arbitrary value
     min_x, min_y = 1, 1
     # Until the path to the finish hasn't been found or if there are no more visitable nodes, we iterate
@@ -180,18 +196,22 @@ function updated_dijkstra(graph::Matrix{Vertex}, start_x::Int64, start_y::Int64,
         # Getting the index of the unvisited and closest node to the starting point
         min_x, min_y = min_dist_vert(distances, uncovered_nodes)
 
+        # min_dist = distances[min_x, min_y]
+
         # Update the distances of the pixels around the pixel at index (min_x, min_y) 
-        distances, shortest_paths = update_distances(distances, uncovered_nodes, graph, min_x, min_y, shortest_paths)
+        distances, parents = update_distances(distances, uncovered_nodes, graph, min_x, min_y, parents)
         # marking the node we just worked around as visited
         uncovered_nodes[min_x, min_y] = true
     end
     println("Distance from vertex (",start_x, ", ", start_y, ") to vertex (", finish_x, ", ", finish_y, "): ", distances[finish_x, finish_y])
     println("Path from (",start_x, ", ", start_y, "): ")
-    for i in 1:size(graph, 1)
-        for j in 1:size(graph, 2)
-            println("to (", i, ", ", j, "): ", shortest_paths[i, j])
-        end
-    end 
+    # for i in 1:size(graph, 1)
+    #     for j in 1:size(graph, 2)
+    #         println("to (", i, ", ", j, "): ", parents[i, j])
+    #     end
+    # end 
+    # printing path from start to end
+    print_parents(parents, finish_x, finish_y)
     
 end
 
@@ -214,10 +234,10 @@ function main()
 
     # Test using an actual .map file
     # Transform the .map file into a matrix of Char
-    mapChar::Matrix{Char} = map_to_matrix("smaller.map")
-    # Transform a Char matrix into a matrix of vertices, corresponding to the graph of the map
-    mapVertices::Matrix{Vertex} = map_to_vertices(mapChar)
+    # mapChar::Matrix{Char} = map_to_matrix("smaller.map")
+    # # Transform a Char matrix into a matrix of vertices, corresponding to the graph of the map
+    # mapVertices::Matrix{Vertex} = map_to_vertices(mapChar)
 
-    updated_dijkstra(mapVertices, 1, 1, 3, 1)
+    # updated_dijkstra(mapVertices, 1, 1, 3, 1)
 
 end
